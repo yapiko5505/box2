af1523699458ecc757f286b1b5e31a7517f7957c
yapiko5505 <bloomingjewels7021@gmail.com>
2021-06-27 16:15:43 +0900

    カレンダー1

---
+++ todo_db_laravel/app/Http/Controllers/CalendarController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Holiday;
+use Illuminate\Http\Request;
+
+class CalendarController extends Controller
+{
+    public function getHoliday(Request $request)
+    {
+        // 休日データ取得
+        $list = Holiday::all();
+        return view('calendar.holiday', ['list' => $list]);
+    }
+
+    public function postHoliday(Request $request)
+    {
+        // POSTで受信した休日データの登録
+        $holiday = new Holiday();
+        $holiday->day = $request->day;
+        $holiday->description = $request->description;
+        $holiday->save();
+        // 休日データ取得
+        $list = Holiday::all();
+        return view('calendar.holiday', ['list' => $list]);
+    }
+}
+
