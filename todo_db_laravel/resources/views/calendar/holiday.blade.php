947e931934d61b0254f73124560f00bd7a1a8ad0
yapiko5505 <bloomingjewels7021@gmail.com>
2021-06-29 13:03:24 +0900

    カレンダー5

--- todo_db_laravel/resources/views/calendar/holiday.blade.php
+++ todo_db_laravel/resources/views/calendar/holiday.blade.php
@@ -1,6 +1,11 @@
 @extends('layout')
 @section('title', '休日設定')
 @section('content')
+    <head>
+    <form name="Logout" method="post" action="/cgi-bin/Logout.cgi">
+        <input type="submit" value="Logout" />
+    </form>
+    </head>
     @if (Auth::check())
         <p>USER: {{$user->name . ' (' . $user->email . ')'}}</p>
     @else<p>※ログインしていません。(<a href="/login">ログイン</a> | <a href="/register">登録</a> )</p>
--- todo_db_laravel/resources/views/calendar/index.blade.php
+++ todo_db_laravel/resources/views/calendar/index.blade.php
@@ -1,10 +1,15 @@
 @extends('layout')
 @section('title', 'カレンダー')
 @section('content')
+<head>
+    <form name="Logout" method="post" action="/cgi-bin/Logout.cgi">
+        <input type="submit" value="Logout" />
+    </form>
+</head>
 @if (Auth::check())
 <p>USER: {{$user->name . ' (' . $user->email . ')'}}</p>
 @else<p>※ログインしていません。(<a href="/login">ログイン</a> | <a href="/register">登録</a> )</p>
+<a href="{{ url('/holiday') }}">休日設定</a>
 @endif
     {!!$cal_tag!!}
 @endsection
-<a href="{{ url('/holiday') }}">休日設定</a>
