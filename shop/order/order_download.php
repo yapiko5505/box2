d5631ace4678c050e2cb6bedf878158e8085e584
yapiko5505 <bloomingjewels7021@gmail.com>
2021-07-11 14:17:58 +0900

    shop7

--- shop/order/order_download.php
+++ shop/order/order_download.php
@@ -14,10 +14,6 @@
 
 ?>
 
-
-
-
-
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -27,10 +23,18 @@
     <title>ろくまる農園</title>
 </head>
 <body>
-    <p>ショップ管理トップメニュー<br></P>
-    <br><a href="../staff/staff_list.php">スタッフ管理</a><br>
-    <br><a href="../product/pro_list.php">商品管理</a><br>
-    <br><a href="../order/order_download.php">注文ダウンロード</a><br>
-    <br><a href="staff_logout.php">ログアウト</a><br>
+    <?php
+        require_once('../kansu/common.php');
+    ?>
+    <p>ダウンロードしたい注文日を選んでください。<br></p>
+    <form method="post" action="order_download_done.php">
+        <?php pulldown_year(); ?>
+        年
+        <?php pulldown_month(); ?>
+        月
+        <?Php pulldown_day(); ?>
+        日<br><br>  
+        <input type="submit" value="ダウンロードへ">
+    </form>
 </body>
 </html>
--- shop/shopping/shop_form_done.php
+++ shop/shopping/shop_form_done.php
@@ -1,7 +1,7 @@
-<?php
-    session_start();
-    session_regenerate_id(true);
-?>
+ <?php
+     session_start();
+     session_regenerate_id(true);
+ ?>
 
 <!DOCTYPE html>
 <html lang="ja">
@@ -60,6 +60,7 @@
 
                 $name=$rec['name'];
                 $price=$rec['price'];
+                $kakaku[]=$price;
                 $suryo=$kazu[$i];
                 $shokei=$price*$suryo;
 
@@ -68,6 +69,46 @@
                 $honbun.=$suryo.'個 =';
                 $honbun.=$shokei."円 ￥n";
             }
+
+                $sql = 'LOCK TABLES dat_sales WRITE, dat_sales_product WRITE';
+                $stmt = $dbh->prepare($sql);
+                $stmt->execute();
+                $sql = 'INSERT INTO dat_sales (code_member, name, email, postal1, postal2, address, tel) VALUES (?, ?, ?, ?, ?, ?, ?)';
+                $stmt = $dbh->prepare($sql);
+                $data = array();
+                $data[] = 0;
+                $data[] = $onamae;
+                $data[] = $email;
+                $data[] = $postal1;
+                $data[] = $postal2;
+                $data[] = $address;
+                $data[] = $tel;
+                $stmt->execute($data);
+
+                $sql = 'SELECT LAST_INSERT_ID()';
+                $stmt = $dbh->prepare();
+                $stmt->execute();
+                $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+                $lastcode = $rec['LAST_INSERT_ID()'];
+
+                for($i=0; $i<$max; $i++)
+                {
+                    $sql = 'INSERT INTO dat_sales (code_member, name, email, postal1, postal2, address, tel) VALUES (?, ?, ?, ?, ?, ?, ?)';
+                    $stmt = $dbh->prepare($sql);
+                    $data = array();
+                    $data[] = 0;
+                    $data[] = $onamae;
+                    $data[] = $email;
+                    $data[] = $postal1;
+                    $data[] = $postal2;
+                    $data[] = $address;
+                    $data[] = $tel;
+                    $stmt->execute($data);
+                }
+
+                $sql= 'UNLOCK TABLES';
+                $stmt = $dbh->prepare($SQl);
+                $stmt->execute();
 
                 $dbh = null;
 
@@ -112,5 +153,6 @@
         exit();
     }
     ?>
+    <br><a href="shop_list.php">商品画面へ</a>
 </body>
 </html>
