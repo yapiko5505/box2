8288901848b9802269455c6c71a3a9187f05878c
yapiko5505 <bloomingjewels7021@gmail.com>
2021-08-05 14:25:02 +0900

    カレンダー11

--- todo_db_laravel/resources/views/calendar/holiday.blade.php
+++ todo_db_laravel/resources/views/calendar/holiday.blade.php
@@ -1,5 +1,5 @@
 @extends('layout')
-@section('title', '休日設定')
+@section('title', 'スケジュール設定')
 @section('content')
     <h1>スケジュール設定</h1>
     <!-- スケジュール入力フォーム -->
@@ -8,7 +8,7 @@
         {{csrf_field()}}  
         <label for="day">日付 [YYYY/MM/DD]</label>
         <input type="text" name="day" class="form-control" id="day" value="{{$data->day}}">
-        <label for="description">説明</label>
+        <label for="description">予定</label>
         <input type="text" name="description" class="form-control" id="description" value="{{$data->description}}"> 
         <div>
         <button type="submit" class="btn btn-primary"> 登録</button>
