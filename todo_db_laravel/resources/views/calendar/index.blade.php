36362af8551c288664482ba3aa992a93ac0c2646
yapiko5505 <bloomingjewels7021@gmail.com>
2021-08-08 07:53:31 +0900

    カレンダー12

--- todo_db_laravel/resources/views/calendar/index.blade.php
+++ todo_db_laravel/resources/views/calendar/index.blade.php
@@ -1,10 +1,6 @@
 @extends('layout')
 @section('title', 'カレンダー')
 @section('content')
-<head>
-    <form name="Logout" method="post" action="/cgi-bin/Logout.cgi">
-    </form>
-</head>
 <a href="{{ url('/holiday') }}">スケジュール設定</a><br>
 <a href="{{ url('/home') }}">ホームからのログアウト</a>
     {!!$cal_tag!!}
