d203d1ca629a14029015584aef412f343c9af185
yapiko5505 <bloomingjewels7021@gmail.com>
2021-08-29 13:17:57 +0900

    laravelapp4

--- laravelapp/app/Http/Controllers/AddressController.php
+++ laravelapp/app/Http/Controllers/AddressController.php
@@ -40,4 +40,41 @@ class AddressController extends Controller
              (:name, :postal, :address, :phone, :email, :todo)', $param);
              return redirect('/address');
         }
+
+        public function edit(Request $request)
+        {
+            $param = ['id' => $request->id];
+            $item = DB::select('select * from address where id = :id', $param);
+            return view('address.edit', ['form' => $item[0]]);
+        }
+
+        public function update(Request $request)
+        {
+            $param = [
+                'id' => $request->id,
+                'name' => $request->name,
+                'postal' => $request->postal,
+                'address' => $request->address,
+                'phone' => $request->phone,
+                'email' => $request->email,
+                'todo' => $request->todo,    
+            ];
+            DB::update('update address set name = :name, postal = :postal, address = :address, phone = :phone, email = :email, todo = :todo where id = :id', $param);
+            return redirect('/address');
+
+        }
+
+        public function delete(Request $request)
+        {
+            $param = ['id' => $request->id];
+            $item = DB::select('select * from address where id = :id', $param);
+            return view('address.delete', ['form' => $item[0]]);
+        }
+
+        public function remove(Request $request)
+        {
+            $param = ['id' => $request->id];
+            DB::delete('delete from address where id = :id', $param);
+            return redirect('/address');
+        }
 }
--- laravelapp/routes/web.php
+++ laravelapp/routes/web.php
@@ -21,6 +21,11 @@ Route::get('/address','App\Http\Controllers\AddressController@index');
 Route::post('/address','App\Http\Controllers\AddressController@post');
 Route::get('/address/add','App\Http\Controllers\AddressController@add');
 Route::post('/address/add','App\Http\Controllers\AddressController@create');
+Route::get('/address/edit','App\Http\Controllers\AddressController@edit');
+Route::post('/address/edit','App\Http\Controllers\AddressController@update');
+Route::get('/address/delete','App\Http\Controllers\AddressController@delete');
+Route::post('/address/delete','App\Http\Controllers\AddressController@remove');
+
 
 
 
